9a34f6c57750c33f52fcd0072b350749ea51809b
unsigned <konndennsa@gmail.com>
2016-10-07 20:36:27 +0900

    write in c++11, add method to read from file

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CXXFLAGS = -O3
+CXXFLAGS = -O3 -std=c++11
 LIBFLAGS = -lm
 CXX = clang++ $(CXXFLAGS)
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include "qcc.hpp"
 
 // WELCOME TO QCC WORLD!!
-// QCC IS A SMALL TOY COMPILER
+// QCC IS A SMALL TOY COMPILER FOR C LANGUAGE
 
-int main() {
+int main(int argc, char *argv[]) {
+  if(argc < 2) {
+    puts("usage: qcc <SOURCE FILE>");
+    return 0;
+  }
   QCC qcc;
+  std::string source = [&]() -> std::string {
+    std::ifstream ifs_src(argv[1]);
+    if(!ifs_src) { puts("file not found"); return ""; }
+    std::istreambuf_iterator<char> it(ifs_src), last;
+    std::string src_all(it, last);
+    return src_all;
+  }();
+  qcc.run(source);
 }
