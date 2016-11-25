492ef8924fd1f2e71df0bcdf1d7f7ee47809f7d2
unsigned <konndennsa@gmail.com>
2016-11-25 14:40:53 +0900

    update lexer

--- lexer.cpp
+++ lexer.cpp
@@ -2,67 +2,79 @@
 #include "token.hpp"
 
 Token Lexer::run(std::string source) {
-  Token token;
-  int cur_line = 1;
-  bool space = false;
+  cur_line = 1;
+  space = false;
 
-  for(int i = 0; i < source.size(); i++) {
-    if(isdigit(source[i])) {
-      std::string number;
-      for(; isdigit(source[i]) || source[i] == '.'; i++)
-        number += source[i];
-      --i; token.add_number_tok(number, cur_line, space);
-      space = false;
-    } else if(source[i] == '_' || isalpha(source[i])) {
-      std::string ident;
-      for(; isalpha(source[i]) || isdigit(source[i]) || 
-          source[i] == '_'; i++)
-        ident += source[i];
-      --i; token.add_ident_tok(ident, cur_line, space);
-      space = false;
-    } else if(isblank(source[i])) { // skip
-      space = true;
-    } else if(source[i] == '/' && source[i+1] == '/') {
-      for(; source[i] != '\n'; i++) {};
-    } else if(source[i] == '/' && source[i+1] == '*') {
-      for(; !(source[i] == '*' && source[i+1] == '/'); i++) { ; }
-      i++;
-    } else if(source[i] == '\"') {
-      std::string content;
-      for(i++; source[i] != '\"' && i < source.size(); i++) {
-        content += source[i];
-        if(source[i] == '\\') 
-          content += source[++i];
-      }
-      token.add_string_tok(content, cur_line, space);
-      space = false;
-    } else if(source[i] == '\'') {
-      if(source[i+1] == '\\') { // TODO: implemet escape sequence
-      } else {
-        i++; std::string ch;ch=source[i];
-        token.add_char_tok(ch, cur_line, space);
-        space = false;
-        i++; // '
-      }
-    } else if(source[i] == '\n') { // skip // TODO: windows support?
-      cur_line++;
-    } else {
-      std::string op; op=source[i];
-      if(source[i+1] == '=' ||
-          (source[i] == '+' && source[i+1] == '+') ||
-          (source[i] == '-' && source[i+1] == '-') ||
-          (source[i] == '&' && source[i+1] == '&') ||
-          (source[i] == '|' && source[i+1] == '|') ||
-          (source[i] == '-' && source[i+1] == '>') ||
-          (source[i] == '.' && source[i+1] == '.') )
-        op += source[++i];
-      if(source[i+1] == '.') op += source[++i];
-      token.add_symbol_tok(op, cur_line, space);
-      space = false;
-    }
+  for(auto pos = source.begin(); pos != source.end(); ++pos) {
+    if(isdigit(*pos))                     tok_number(pos);
+    else if(*pos == '_' || isalpha(*pos)) tok_ident(pos);
+    else if(isblank(*pos))                space = true; // skip
+    else if(*pos == '\\')                 skip_line(pos);
+    else if(*pos == '/' && *(pos+1) == '/') {
+      for(; *pos != '\n'; pos++) {};
+    } else if(*pos == '/' && *(pos+1) == '*') {
+      for(; !(*pos == '*' && *(pos+1) == '/'); pos++) { ; }
+      pos++;
+    } else if(*pos == '\"')               tok_string(pos);
+    else if(*pos == '\'')                 tok_char(pos);
+    else if(*pos == '\n')                 skip_line(pos), cur_line++;
+    else                                  tok_symbol(pos);
   } 
 
   token.add_end_tok();
 
   return token;
+}
+
+void Lexer::tok_number(std::string::iterator &pos) {
+  std::string number;
+  for(; isdigit(*pos) || *pos == '.'; pos++)
+    number += *pos;
+  --pos; token.add_number_tok(number, cur_line, space);
+  space = false;
+}
+void Lexer::tok_ident(std::string::iterator &pos) {
+  std::string ident;
+  for(; isalpha(*pos) || isdigit(*pos) || 
+      *pos == '_'; pos++)
+    ident += *pos;
+  --pos; token.add_ident_tok(ident, cur_line, space);
+  space = false;
+}
+void Lexer::tok_string(std::string::iterator &pos) {
+  std::string content;
+  for(pos++; *pos != '\"'; pos++) {
+    content += *pos;
+    if(*pos == '\\') 
+      content += *++pos;
+  }
+  token.add_string_tok(content, cur_line, space);
+  space = false;
+}
+void Lexer::tok_char(std::string::iterator &pos) {
+  if(*(pos+1) == '\\') { // TODO: implemet escape sequence
+  } else {
+    pos++; std::string ch;ch=*pos;
+    token.add_char_tok(ch, cur_line, space);
+    space = false;
+    pos++; // '
+  }
+}
+void Lexer::tok_symbol(std::string::iterator &pos) {
+  std::string op; op = *pos;
+  if(                 *(pos+1) == '='  ||
+      (*pos == '+' && *(pos+1) == '+') ||
+      (*pos == '-' && *(pos+1) == '-') ||
+      (*pos == '&' && *(pos+1) == '&') ||
+      (*pos == '|' && *(pos+1) == '|') ||
+      (*pos == '-' && *(pos+1) == '>') ||
+      (*pos == '.' && *(pos+1) == '.') )
+    op += *++pos;
+  if(*(pos+1) == '.') op += *++pos;
+  token.add_symbol_tok(op, cur_line, space);
+  space = false;
+}
+
+void Lexer::skip_line(std::string::iterator &pos) {
+  while(*pos != '\n') pos++;
 }
--- lexer.hpp
+++ lexer.hpp
@@ -4,6 +4,17 @@
 #include "token.hpp"
 
 class Lexer {
+  private:
+    int cur_line = 1;
+    bool space = false;
+    Token token;
+
+    void tok_number(std::string::iterator &);
+    void tok_ident (std::string::iterator &);
+    void tok_string(std::string::iterator &);
+    void tok_char  (std::string::iterator &);
+    void tok_symbol(std::string::iterator &);
+    void skip_line (std::string::iterator &);
   public:
     Token run(std::string);
 };
