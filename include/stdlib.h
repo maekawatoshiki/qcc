da78e1d285e513e41e5a25c8af59a86ee1a28429
maekawatoshiki <konndennsa@gmail.com>
2016-12-04 22:20:57 +0900

    add new header file

---
+++ include/stdlib.h
@@ -0,0 +1,11 @@
+typedef int size_t;
+double atof(char *);
+int atoi(char *);
+double strtod(char *, char **);
+void *calloc(size_t nitems, size_t size);
+void free(void *);
+void *malloc(size_t);
+void *realloc(void *, size_t);
+int abs(int);
+int rand();
+void srand(int);
