4bd6dc4576215dc03e5cfdb9cb59af5985eaeb57
equal-l2 <equal-l2@users.noreply.github.com>
2017-01-09 00:54:10 +0900

    Remove unnecessary include

--- src/common.hpp
+++ src/common.hpp
@@ -1,21 +1,6 @@
 #pragma once
 
-#include <llvm/Analysis/Passes.h>
-#include <llvm/IR/Verifier.h>
-#include <llvm/ExecutionEngine/ExecutionEngine.h>
-#include <llvm/IR/DataLayout.h>
-#include <llvm/IR/DerivedTypes.h>
 #include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/LLVMContext.h>
-#include <llvm/IR/GlobalVariable.h>
-#include <llvm/IR/Module.h>
-#include <llvm/Support/SourceMgr.h>
-#include <llvm/Linker/Linker.h>
-#include <llvm/Support/MemoryBuffer.h>
-#include <llvm/IRReader/IRReader.h>
-#include <llvm/PassManager.h>
-#include <llvm/Support/TargetSelect.h>
-#include <llvm/Transforms/Scalar.h>
 #include <llvm/Bitcode/ReaderWriter.h>
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/raw_ostream.h>
