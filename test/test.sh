a021ef7f784059e3f1421cf2307fcfffd9f0adf2
equal-l2 <equal-l2@users.noreply.github.com>
2017-02-08 18:09:23 +0900

    Test LLVM 3.8

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,4 @@
+echo $1
 ./qcc $1.c -o $1.bc > /dev/null
-llc-3.6 $1.bc -o $1.s
-clang $1.s -D"TEST_NAME=\"$1\"" test/main.c -o $1.bin
+llc-3.8 $1.bc -o $1.s
+clang-3.8 $1.s -D"TEST_NAME=\"$1\"" test/main.c -o $1.bin
